1b9856a1613126c3c1895e1bd41f268daa5eb521
jairomartinez1994 <61437470+jairomartinez1994@users.noreply.github.com>
2020-08-12 19:56:28 -0400

    Updates

---
+++ precache-manifest.464663c1f902592095fd4d80d7337d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2b51021c6ab5f1d507e41f168749bb",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "1fc72b56974d8030ad3b",
+    "url": "/React-Employee-Directory/static/css/main.ea969335.chunk.css"
+  },
+  {
+    "revision": "59863d3c070bc896ca89",
+    "url": "/React-Employee-Directory/static/js/2.052e066c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.052e066c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc72b56974d8030ad3b",
+    "url": "/React-Employee-Directory/static/js/main.7fd8cd1e.chunk.js"
+  },
+  {
+    "revision": "926bfc54df9bd7faaaa6",
+    "url": "/React-Employee-Directory/static/js/runtime-main.ce816f18.js"
+  }
+]);
